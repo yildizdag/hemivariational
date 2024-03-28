6dea457ee7a825b22a1f9052910bf8891e650414
yildizdag <yildizdag@berkeley.edu>
2024-03-28 22:01:31 +0100

    single spring epd done

--- e_epd.m
+++
@@ -1,4 +0,0 @@
-function etot = e_epd(k,kd,kt,st,sc,Kp,ui,u0)
-etot = 0.5*k*(1-u0(2))*(u0(1)-u0(3)+u0(4))^2+0.5*kd*u0(2)^2+kt*u0(2)+st*u0(3)+sc*u0(4)+0.5*Kp*(u0(1)-ui)^2;
-end
-
--- energy_elastodamage.m
+++
@@ -1,4 +0,0 @@
-function etot = energy_elastodamage(k,kd,kt,Kp,ui,u,d)
-etot = 0.5*k*(1-d)*u^2+0.5*Kp*(u-ui)^2+0.5*kd*d^2+kt*d;
-end
-
